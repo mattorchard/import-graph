75a549eda35e4c32403253cd53c30d651a87d20b
Matthew <morch028@uottawa.ca>
2023-02-18 16:17:30 -0500

    Fix imports with valid extension being unmatched

--- src/utilities/ImportResolver.ts
+++ src/utilities/ImportResolver.ts
@@ -1,15 +1,30 @@
-import { isNotOneOf } from "../helpers/CollectionHelpers";
+import { isNotOneOf, isOneOf } from "../helpers/CollectionHelpers";
+import { getFileExtension, removeFileExtension } from "../helpers/FileHelpers";
 
 export class ImportResolver {
   private readonly separator = "/";
+  private readonly isAllowedFileExtension: (item: string | null) => boolean;
+
+  constructor(allowedFileExtensions: Array<string | null>) {
+    this.isAllowedFileExtension = isOneOf(allowedFileExtensions);
+  }
 
   public resolve(startPath: string[], rawImportPath: string): string[] | null {
     const importParts = rawImportPath.split(this.separator);
     // Probably a package import, should be more specific once aliases supported
     if (importParts.every(isNotOneOf([".", ".."]))) return null;
 
+    const fileName = importParts.at(-1);
+    if (!fileName || !this.isAllowedFileExtension(getFileExtension(fileName)))
+      return null;
+
+    const allImportSegments = [
+      ...startPath,
+      ...importParts.slice(0, 1),
+      removeFileExtension(fileName),
+    ];
     const resolvedPath = new Array<string>();
-    for (const part of [...startPath, ...importParts]) {
+    for (const part of allImportSegments) {
       switch (part) {
         case ".":
           break;
